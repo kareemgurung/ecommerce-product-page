ea6d9ab8f8acb2f331276c51f4b5bf40086e9f00
Kareem Gurung <66091354+kareemgurung@users.noreply.github.com>
2023-08-23 00:12:56 +0100

    test

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import styles from "./button.module.css";
 
 const Button = ({ type, text, img, onClick }) => {
   return (
-    <button className={styles.button} type={type} onClick={onClick} title={text}>
+    <button className={styles.button} type={type} onClick={onClick} aria-label={text}>
       {img && <img src={img} alt="" />}
       {text}
     </button>
