3d7835d8e95b9646230a31dfd01df985bd0774cd
Kareem Gurung <66091354+kareemgurung@users.noreply.github.com>
2023-08-23 00:09:41 +0100

    added title to button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import styles from "./button.module.css";
 
 const Button = ({ type, text, img, onClick }) => {
   return (
-    <button className={styles.button} type={type} onClick={onClick}>
+    <button className={styles.button} type={type} onClick={onClick} title={text}>
       {img && <img src={img} alt="" />}
       {text}
     </button>
