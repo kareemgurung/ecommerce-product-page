cb6389ef97f6db269b0e9f6ad494ef427f8d659a
Kareem Gurung <66091354+kareemgurung@users.noreply.github.com>
2023-08-22 12:32:32 +0100

    cart component completed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,22 +1,77 @@
+import { useContext } from "react";
 import styles from "./cart.module.css";
+import CartContext from "../../context/CartContext";
+import Button from "../Button/Button";
 
-const Cart = ({ quantity }) => {
+const Cart = () => {
+  const ctx = useContext(CartContext);
+
+  const isEmpty = ctx.finalCount === 0;
+
+  if (isEmpty) {
+    return (
+      <div className={styles.container}>
+        <h3 className={styles.title}>Cart</h3>
+        <hr className={styles.underline} />
+        <p className={styles.empty}>Your cart is empty</p>
+      </div>
+    );
+  }
+
   return (
-    <button>
-      <span>{quantity}</span>
-      <svg
-        className={styles.cartIcon}
-        width="22"
-        height="20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
-          fill="#69707D"
-          fill-rule="nonzero"
+    <div className={styles.container}>
+      <h3 className={styles.title}>Cart</h3>
+      <hr className={styles.underline} />
+      <div className={styles.basket}>
+        <div className={styles.item}>
+          <img src="./images/image-product-1-thumbnail.jpg" alt="" />
+          <div className={styles.desc}>
+            <p>Fall Limited Edition Sneakers</p>
+            <div className={styles.price}>
+              <p>$125.00 x 3</p>
+              <p className={styles.total}>$375.00</p>
+            </div>
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="16"
+            viewBox="0 0 14 16"
+            fill="none"
+            className={styles.cart}
+            onClick={ctx.emptyCart}
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M0 2.625V1.75C0 1.33438 0.334375 1 0.75 1H4.25L4.54375 0.415625C4.66875 0.159375 4.92812 0 5.2125 0H8.78438C9.06875 0 9.32812 0.159375 9.45625 0.415625L9.75 1H13.25C13.6656 1 14 1.33438 14 1.75V2.625C14 2.83125 13.8313 3 13.625 3H0.375C0.16875 3 0 2.83125 0 2.625ZM13 4.375V14.5C13 15.3281 12.3281 16 11.5 16H2.5C1.67188 16 1 15.3281 1 14.5V4.375C1 4.16875 1.16875 4 1.375 4H12.625C12.8313 4 13 4.16875 13 4.375ZM4 6C4.275 6 4.5 6.225 4.5 6.5V13.5C4.5 13.775 4.275 14 4 14C3.725 14 3.5 13.775 3.5 13.5V6.5C3.5 6.225 3.725 6 4 6ZM7.5 6.5C7.5 6.225 7.275 6 7 6C6.725 6 6.5 6.225 6.5 6.5V13.5C6.5 13.775 6.725 14 7 14C7.275 14 7.5 13.775 7.5 13.5V6.5ZM10 6C10.275 6 10.5 6.225 10.5 6.5V13.5C10.5 13.775 10.275 14 10 14C9.725 14 9.5 13.775 9.5 13.5V6.5C9.5 6.225 9.725 6 10 6Z"
+              fill="#C3CAD9"
+            />
+            <mask
+              id="mask0_0_632"
+              maskUnits="userSpaceOnUse"
+              x="0"
+              y="0"
+              width="14"
+              height="16"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M0 2.625V1.75C0 1.33438 0.334375 1 0.75 1H4.25L4.54375 0.415625C4.66875 0.159375 4.92812 0 5.2125 0H8.78438C9.06875 0 9.32812 0.159375 9.45625 0.415625L9.75 1H13.25C13.6656 1 14 1.33438 14 1.75V2.625C14 2.83125 13.8313 3 13.625 3H0.375C0.16875 3 0 2.83125 0 2.625ZM13 4.375V14.5C13 15.3281 12.3281 16 11.5 16H2.5C1.67188 16 1 15.3281 1 14.5V4.375C1 4.16875 1.16875 4 1.375 4H12.625C12.8313 4 13 4.16875 13 4.375ZM4 6C4.275 6 4.5 6.225 4.5 6.5V13.5C4.5 13.775 4.275 14 4 14C3.725 14 3.5 13.775 3.5 13.5V6.5C3.5 6.225 3.725 6 4 6ZM7.5 6.5C7.5 6.225 7.275 6 7 6C6.725 6 6.5 6.225 6.5 6.5V13.5C6.5 13.775 6.725 14 7 14C7.275 14 7.5 13.775 7.5 13.5V6.5ZM10 6C10.275 6 10.5 6.225 10.5 6.5V13.5C10.5 13.775 10.275 14 10 14C9.725 14 9.5 13.775 9.5 13.5V6.5C9.5 6.225 9.725 6 10 6Z"
+                fill="white"
+              />
+            </mask>
+            <g mask="url(#mask0_0_632)"></g>
+          </svg>
+        </div>
+        <Button
+          type="button"
+          text="Checkout"
+          onClick={() => console.log("checked out")}
         />
-      </svg>
-    </button>
+      </div>
+    </div>
   );
 };
 
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -2,27 +2,24 @@ import { createContext, useState } from "react";
 
 const CartContext = createContext({
   productCount: 0,
+  finalCount: 0,
   increaseCount: () => {},
   decreaseCount: () => {},
   addToCart: () => {},
+  showCart: false,
+  toggleCart: () => {},
+  emptyCart: () => {},
 });
 
 export const CartContextProvider = (props) => {
   const [productCount, setProductCount] = useState(0);
   const [finalCount, setFinalCount] = useState(0);
-
-//   useEffect(() => {
-//     setFinalCount(finalCount + productCount)
-//     setProductCount(0)
-//   }, [productCount]) 
+  const [showCart, setShowCart] = useState(true);
 
   const addToCartHandler = () => {
     const newCount = finalCount + productCount;
     setFinalCount(newCount);
-    console.log(finalCount);
-    console.log(productCount);
     setProductCount(0);
-    console.log(productCount);
   };
 
   const increaseCountHandler = () => {
@@ -36,13 +33,26 @@ export const CartContextProvider = (props) => {
     const newCount = productCount - 1;
     setProductCount(newCount);
   };
+
+  const cartHandler = () => {
+    setShowCart((prev) => !prev);
+  };
+
+  const emptyCartHandler = () => {
+    setFinalCount(0);
+  };
+
   return (
     <CartContext.Provider
       value={{
         productCount: productCount,
+        finalCount: finalCount,
         increaseCount: increaseCountHandler,
         decreaseCount: decreaseCountHandler,
         addToCart: addToCartHandler,
+        showCart: showCart,
+        toggleCart: cartHandler,
+        emptyCart: emptyCartHandler,
       }}
     >
       {props.children}
