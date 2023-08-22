8201da9234b967b30290469fe1b881b79101945f
Kareem Gurung <66091354+kareemgurung@users.noreply.github.com>
2023-08-23 00:37:51 +0100

    button border changed

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,8 +2,13 @@ import styles from "./button.module.css";
 
 const Button = ({ type, text, img, onClick }) => {
   return (
-    <button className={styles.button} type={type} onClick={onClick} aria-label={text}>
-      {img && <img src={img} alt="" />}
+    <button
+      className={styles.button}
+      type={type}
+      onClick={onClick}
+      aria-label={text}
+    >
+      {img && <img src={img} alt="cart icon" />}
       {text}
     </button>
   );
